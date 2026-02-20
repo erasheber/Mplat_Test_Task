c9e982b4f087d79a883169a3dede753efc65f918
Конаев Ерасыл <yerassylkonaev@gmail.com>
2026-02-20 17:10:31 +0500

    Changed Program.cs and added ConnectionString for the database

--- backend/Dotnet_Test_Task.Api/Program.cs
+++ backend/Dotnet_Test_Task.Api/Program.cs
@@ -1,6 +1,19 @@
+using Dotnet_Test_Task.Infrastructure.Persistence;
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PaymentsDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddFastEndpoints();
+builder.Services.SwaggerDocument();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseFastEndpoints();
+app.UseSwaggerGen();
 
 app.Run();
