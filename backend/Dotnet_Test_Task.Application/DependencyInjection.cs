29b53fc2c7596d233ba073676bc1bee10efa2ae1
Конаев Ерасыл <yerassylkonaev@gmail.com>
2026-02-22 02:01:26 +0500

    Added DI Bootstrapper for the Application and Infrastructure layers

---
+++ backend/Dotnet_Test_Task.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Dotnet_Test_Task.Application.Interfaces;
+using Dotnet_Test_Task.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dotnet_Test_Task.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IPaymentsService, PaymentsService>();
+        return services;
+    }
+}
---
+++ backend/Dotnet_Test_Task.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Dotnet_Test_Task.Application.Interfaces;
+using Dotnet_Test_Task.Infrastructure.Persistence;
+using Dotnet_Test_Task.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dotnet_Test_Task.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration cfg)
+    {
+        services.AddDbContext<PaymentsDbContext>(opt =>
+            opt.UseNpgsql(cfg.GetConnectionString("Default")));
+
+        services.AddScoped<IPaymentsRepository, PaymentsRepository>();
+
+        return services;
+    }
+}
