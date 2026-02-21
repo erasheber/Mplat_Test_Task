1b69298cca44cdc8866c87e12858ff07fd54dde8
Конаев Ерасыл <yerassylkonaev@gmail.com>
2026-02-22 02:04:21 +0500

    Wire DI and API key middleware, now testable in the swagger

--- backend/Dotnet_Test_Task.Api/Program.cs
+++ backend/Dotnet_Test_Task.Api/Program.cs
@@ -1,20 +1,47 @@
 using Dotnet_Test_Task.Api.Security;
+using Dotnet_Test_Task.Application;
+using Dotnet_Test_Task.Infrastructure;
 using Dotnet_Test_Task.Infrastructure.Persistence;
 using FastEndpoints;
 using FastEndpoints.Swagger;
 using Microsoft.EntityFrameworkCore;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<ApiKeyOptions>(builder.Configuration.GetSection("Security"));
 
-builder.Services.AddDbContext<PaymentsDbContext>(opt =>
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddFastEndpoints();
-builder.Services.SwaggerDocument();
+builder.Services.SwaggerDocument(o =>
+{
+    o.DocumentSettings = s =>
+    {
+        s.Title = "Dotnet_Test_Task API";
+
+        s.AddSecurity("ApiKey", [], new OpenApiSecurityScheme
+        {
+            Type = OpenApiSecuritySchemeType.ApiKey,
+            Name = "X-Api-Key",
+            In = OpenApiSecurityApiKeyLocation.Header,
+            Description = "API key needed to access endpoints"
+        });
+
+        s.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("ApiKey"));
+    };
+});
 
 var app = builder.Build();
+
+if (app.Configuration.GetValue<bool>("Database:AutoMigrate"))
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<PaymentsDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseMiddleware<ApiKeyMiddleware>();
 
