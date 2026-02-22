7b4c95e776faf28cc65f8e9fef80b1b3641a9d2a
Конаев Ерасыл <yerassylkonaev@gmail.com>
2026-02-23 04:08:44 +0500

    Managed routes with vue-router

---
+++ frontend/src/router.js
@@ -0,0 +1,16 @@
+﻿import { createRouter, createWebHistory } from "vue-router";
+import CreatePaymentView from "./views/CreatePaymentView.vue";
+import PaymentsHistoryView from "./views/PaymentsHistoryView.vue";
+import PaymentsStatsView from "./views/PaymentsStatsView.vue";
+
+const routes = [
+    { path: "/", redirect: "/create" },
+    { path: "/create", component: CreatePaymentView },
+    { path: "/history", component: PaymentsHistoryView },
+    { path: "/stats", component: PaymentsStatsView },
+];
+
+export default createRouter({
+    history: createWebHistory(),
+    routes,
+});
