fbb0fdb3b07c5fa4f37d51c00c11660528416fa4
Конаев Ерасыл <yerassylkonaev@gmail.com>
2026-02-23 04:08:18 +0500

    Added API endpoint call functions

---
+++ frontend/src/api/payments.js
@@ -0,0 +1,16 @@
+﻿import http from "./http";
+
+export async function createPayment(payload) {
+    const { data } = await http.post("/api/payments", payload);
+    return data;
+}
+
+export async function getPayments(params) {
+    const { data } = await http.get("/api/payments", { params });
+    return data;
+}
+
+export async function getStats(params) {
+    const { data } = await http.get("/api/payments/stats", { params });
+    return data;
+}
