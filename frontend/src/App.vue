d4b1553c45b03be4237996a2ed27245b2c7454d7
Конаев Ерасыл <yerassylkonaev@gmail.com>
2026-02-23 04:04:20 +0500

    init Vue 3 app

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <div class="container">
+      <NavBar />
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import NavBar from "./components/Navbar.vue";
+</script>
+
+<style scoped>
+.wrap { max-width: 980px; margin: 0 auto; padding: 16px; }
+</style>
